3f1bb28d00f736ca53c3db9628e5b4e26ab5eb8c
Meet Thummar <meetthummar132@gmail.com>
2025-05-07 00:17:52 +0530

    p1

--- scrape.js
+++ scrape.js
@@ -1,17 +1,29 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
+const { load } = require('cheerio');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const pointSchema = new mongoose.Schema({
-    data:Object
+    data:{
+        type: Object,
+        required: true
+    }
 },{timestamps:true});
 
 const PointsTable = mongoose.model('PointsTable', pointSchema);
 
 async function scrape() {
+    const headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Upgrade-Insecure-Requests': '1',
+        'DNT': '1',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8,en-GB;q=0.6,en-AU;q=0.4'
+    }
     const url="https://www.espncricinfo.com/series/ipl-2025-1449924/points-table-standings";
         const response = await axios.get(url, { headers });
         const $ = load(response.data);
@@ -44,7 +56,7 @@ async function scrape() {
         })
         await mongoose.connect(MONGODB_URI);
         await PointsTable.deleteMany({});
-        await PointsTable.create(pointsTable);
+        await PointsTable.create({data:pointsTable});
         await mongoose.disconnect();        
 }
 
